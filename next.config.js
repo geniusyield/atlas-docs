15592e7ab4d931ede8372608183167897c91ddcb
Varderes Barsegyan <barsegyanvarderes@gmail.com>
2023-03-22 10:56:55 -0500

    fix them config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
-// const withNextra = require('nextra')({
-//     theme: 'nextra-theme-docs',
-//     themeConfig: './theme.config.jsx',
-//   })
-   
-//   module.exports = withNextra()
-   
-//   // If you have other Next.js configurations, you can pass them as the parameter:
-//   // module.exports = withNextra({ /* other next.js config */ })
-
-// next.config.js
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = {
-  assetPrefix: isProd ? 'atlas-docs' : '',
-  images: {
-    unoptimized: true,
-  },
-}
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+    assetPrefix: isProd ? 'atlas-docs' : '',
+    images: {
+      unoptimized: true,
+    },
+  })
+   
+  module.exports = withNextra()
+   
