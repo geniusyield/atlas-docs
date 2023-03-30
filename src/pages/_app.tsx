a0265734c4f992783645d2edddf39712c88c31e0
Haluk-GeniusYield <haluk@geniusyield.co>
2023-03-30 17:52:05 +0300

    Added helmet config to Atlas #39

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
-import { useState } from "react";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { Helmet } from "react-helmet";
 
 import "../styles/globals.css";
 
@@ -13,6 +13,27 @@ function App({ Component, pageProps }: AppProps) {
 
   return (
     <ThemeProvider theme={theme}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Your Website Title</title>
+        <meta name="description" content="Your website description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="
+            default-src 'self';
+            img-src 'self' data: *.google-analytics.com *.google.co.uk *.google.com *.googleusercontent.com *.gstatic.com;
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' www.google.com *.google.com *.googleapis.com *.google-analytics.com *.googletagmanager.com;
+            connect-src 'self' *.google-analytics.com analytics.google.com *.analytics.google.com doubleclick.net *.doubleclick.net *.google.com;
+            style-src 'self' 'unsafe-inline';
+          "
+        />
+        <meta httpEquiv="X-Content-Type-Options" content="nosniff" />
+        <meta httpEquiv="X-Frame-Options" content="SAMEORIGIN" />
+        <meta httpEquiv="X-XSS-Protection" content="1; mode=block" />
+        <meta httpEquiv="Referrer-Policy" content="no-referrer" />
+      </Helmet>
       <Component {...pageProps} />
     </ThemeProvider>
   );
