a4886cfafba40fe961abce885ed12f44bfee2a88
dacomis <albei.andrei@gmail.com>
2023-03-30 11:42:18 +0300

    Add GTM and id to a button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,37 @@
 import type { AppProps } from "next/app";
-import { useState } from "react";
+import Script from "next/script";
 import { createTheme, ThemeProvider } from "@mui/material";
+
+import { getTheme } from "../lib/theme/theme";
 
 import "../styles/globals.css";
 
-import { getTheme } from "../lib/theme/theme";
-import { mutateFontSizeResponsiveness } from "../lib/theme/responsiveTypography";
-
 function App({ Component, pageProps }: AppProps) {
   const theme = createTheme(getTheme());
-  // theme = mutateFontSizeResponsiveness(theme);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script
+        id="tagmanager-main"
+        strategy="afterInteractive"
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id="GTM-PVJFBVL"`}></Script>
+      <Script
+        id="tagmanager-setup"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GTM-PVJFBVL');
+          `,
+        }}
+      />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 export default App;
