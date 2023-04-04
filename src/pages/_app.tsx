74899fa8797eb42f76470819d19c9fa247db6f2c
Dacomis <45426359+Dacomis@users.noreply.github.com>
2023-04-04 09:46:29 +0300

    Change GTM Implementation (#51)
    * Change GTM implementation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Script from "next/script";
 import { createTheme, ThemeProvider } from "@mui/material";
 
+import { pageview } from "../lib/gtm/gtm";
 import { getTheme } from "../lib/theme/theme";
 
 import "../styles/globals.css";
@@ -9,24 +12,27 @@ import "../styles/globals.css";
 function App({ Component, pageProps }: AppProps) {
   const theme = createTheme(getTheme());
 
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", pageview);
+    return () => {
+      router.events.off("routeChangeComplete", pageview);
+    };
+  }, [router.events]);
+
   return (
     <>
       {/* GTM Integration */}
       <Script
-        id="tagmanager-main"
-        strategy="afterInteractive"
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id="GTM-PVJFBVL"`}
-      />
-      <Script
-        id="tagmanager-setup"
+        id="gtag"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'GTM-PVJFBVL');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'GTM-PVJFBVL');
           `,
         }}
       />
